af34489a5b13370898b3d7963929ffdc788ea740
Lucas Barbosa <60528589+barbosalucas278@users.noreply.github.com>
2025-10-08 00:42:53 -0300

    revert envs

--- auth0-dashboard/src/index.jsx
+++ auth0-dashboard/src/index.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
-const domain = import.meta.env.AUTH0_DOMAIN;
-const clientId = import.meta.env.AUTH0_M2M_CLIENT_ID;
+const domain = import.meta.env.VITE_AUAUTH0_DOMAINTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENTID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
